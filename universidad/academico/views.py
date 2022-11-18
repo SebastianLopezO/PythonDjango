5e29c138a5cc04089570013e06f26c193ad5e347
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2022-11-18 14:45:57 -0500

    main

--- universidad/academico/views.py
+++ universidad/academico/views.py
@@ -5,8 +5,9 @@ from academico.models import Facultad
 # Create your views here.
 
 def facultades(request) :
-    facultad = Facultad.objects.all()
+    facultad = Facultad.objects.values_list()
+    schema = Facultad._meta.fields
     actual = datetime.now()
     # contexto
-    context = {"facultad":facultad, "actual":actual}
+    context = {"facultad":facultad, "actual":actual,"schema":schema}
     return render(request, "facultades.html", context)
